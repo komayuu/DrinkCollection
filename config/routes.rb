d909e65c41f9fb1c383491b2818cc3980d3c3536
小松優誠 <yuseikomatsu@yuseis-PC.local>
2024-12-03 00:28:29 +0900

    トップページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,5 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "tasks#index"
+  root "static_pages#top"
 end
