5434f03ae52a90ee2ddb1b7e3630316cb6afe660
小松優誠 <yuseikomatsu@yuseis-PC.local>
2024-12-16 23:22:03 +0900

    記事の投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "static_pages#top"
   # ユーザー処理
   resources :users, only: %i[new create show edit update]
-  # 投稿一覧
-  resources :posts, only: %i[index]
+  # 投稿一覧、投稿作成
+  resources :posts, only: %i[index new create]
   # ログインアウト処理
   get "login", to: "user_sessions#new"
   post "login", to: "user_sessions#create"
