62d66406dace1c342d49b42654328805fb9d4e1f
小松優誠 <yuseikomatsu@yuseis-PC.local>
2025-02-16 15:18:56 +0900

    自動ログイン機能の実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,9 +4,10 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:email], params[:password], params[:remember_me])
 
     if @user
+      remember_me! if params[:remember_me] == "1" # チェックボックスがオンの時remember_meを有効化
       redirect_to root_path, success: "ログインしました"
     else
       flash.now[:danger] = "ログインに失敗しました"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_16_183604) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_16_054325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_16_183604) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_admin", default: false, null: false
+    t.string "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
