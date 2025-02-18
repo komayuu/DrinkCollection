6a0d5a79475b3a5ca75c4112c58beff6a3b8b1c5
小松優誠 <yuseikomatsu@yuseis-PC.local>
2025-02-19 02:35:48 +0900

    X共有機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-  # 値が空でないこと・最大35文字以下であること
-  validates :title, presence: { message: "を入力してください" }, length: { maximum: 35, message: "は35文字以内で入力してください" }
-  # 値が空でないこと・最大270文字以下であること
-  validates :body, presence: { message: "を入力してください" }, length: { maximum: 270, message: "は270文字以内で入力してください" }
+  # 値が空でないこと・最大30文字以下であること
+  validates :title, presence: { message: "を入力してください" }, length: { maximum: 30, message: "は35文字以内で入力してください" }
+  # 値が空でないこと・最大250文字以下であること
+  validates :body, presence: { message: "を入力してください" }, length: { maximum: 250, message: "は270文字以内で入力してください" }
 
 
   belongs_to :user
