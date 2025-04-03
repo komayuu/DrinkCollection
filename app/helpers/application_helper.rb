faffaa23b37f0abc8ae46323f30eae8250f87fe7
小松優誠 <yuseikomatsu@yuseis-PC.local>
2025-04-03 14:54:48 +0900

    デプロイエラー対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
       site: "DrinkCollection",
       title: "新たなお酒との出会い",
       reverse: true,
-      charset: "utf-8"
-      separator: "|",   #Webサイト名とページタイトルを区切るために使用されるテキスト
+      charset: "utf-8",
+      separator: "|",   # Webサイト名とページタイトルを区切るために使用されるテキスト
       description: "お酒好きや興味のある方にぴったりのwebアプリ!",
       keywords: "お酒,飲み物,Drink,雰囲気",
       canonical: "https://drinkcollection-1.onrender.com",
@@ -30,7 +30,7 @@ module ApplicationHelper
         site: "@snowflowerhaku",
         title: "DrinkCollection",
         description: "お酒好きや興味のある方にぴったりのwebアプリ!",
-        image: image_url("ogp-image.png")
+        image: image_url("ogp-image.png"),
         url: "https://drinkcollection-1.onrender.com"
       }
     }
